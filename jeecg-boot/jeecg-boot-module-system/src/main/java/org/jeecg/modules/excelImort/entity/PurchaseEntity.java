3879a4b5ffd76a065cefc209c54aba663d776109
furenjie <frj9641@163.com>
2021-11-16 14:49:22 +0800

    test

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/excelImort/entity/PurchaseEntity.java
@@ -0,0 +1,26 @@
+package org.jeecg.modules.excelImort.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PurchaseEntity {
+    private String id;
+    private String djh;
+    private String site;
+    private String material;
+    private String supply;
+    private String slT;
+    private String cgdj;
+    private String yfdj;
+    private String zje;
+    private String purchaseDate;
+    private String creater;
+    private String remark;
+    private String checkFlag;
+    private String sysDate;
+    private String checkDate;
+    private String rkFlag;
+    private String purchaseWay;
+}
