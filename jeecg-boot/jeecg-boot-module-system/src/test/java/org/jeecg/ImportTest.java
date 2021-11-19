d0b514be05ea35222bfbebbf4821e4966d8585ef
faylpf <317126370@qq.com>
2021-11-19 18:01:19 +0800

    '质量数据采集——主附表导入'

--- jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/ImportTest.java
+++ jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/ImportTest.java
@@ -79,5 +79,20 @@ public class ImportTest {
         excelWorker.importExcel(fileInputStream, 0);
     }
 
+    @Test
+    public void testWaterDataCollectMain() throws FileNotFoundException {
+        InputStream fileInputStream = new FileInputStream("E:\\IdeaProject\\hte_erp_new\\jeecg-boot\\jeecg-boot-module-system\\src\\main\\java\\org\\jeecg\\modules\\excel\\water_data_collect_main.xlsx");
+        System.out.println(excelWorker);
+        excelWorker.importExcel(fileInputStream, 0);
+    }
+
+    @Test
+    public void testWaterDataCollectDetail() throws FileNotFoundException {
+        InputStream fileInputStream = new FileInputStream("E:\\IdeaProject\\hte_erp_new\\jeecg-boot\\jeecg-boot-module-system\\src\\main\\java\\org\\jeecg\\modules\\excel\\water_data_collect_detail" +
+                ".xlsx");
+        System.out.println(excelWorker);
+        excelWorker.importExcel(fileInputStream, 0);
+    }
+
 
 }
