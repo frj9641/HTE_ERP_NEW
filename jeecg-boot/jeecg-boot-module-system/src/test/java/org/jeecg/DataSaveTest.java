5c54fcc47ccf6767d68cb1c396e494adf68d8ff8
furenjie <frj9641@163.com>
2021-12-15 11:23:37 +0800

    插入detail_port_monthly的脚本完成

--- jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/DataSaveTest.java
+++ jeecg-boot/jeecg-boot-module-system/src/test/java/org/jeecg/DataSaveTest.java
@@ -23,8 +23,10 @@ public class DataSaveTest {
     @Autowired
     private DataCollectSelectJobMapper dataCollectSelectJobMapper;
 
+    private String[] index = {"一", "二", "三", "四", "五", "六", "七", "八", "九", "十"};
+
     @Test
-    public void savePortDetailMonthly(String key1, String type, String key2) throws ParseException {
+    public void savePortDetailMonthly() throws ParseException {
         Calendar c = Calendar.getInstance();
         SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
         Date beginDate = dateFormat1.parse("2018-08-01");
@@ -33,7 +35,17 @@ public class DataSaveTest {
         while (!date.equals(endDate)) {
             String[] timeZone = ckProductDealJob.getTimeZone(date);
             List<Map<String, Object>> list = dataCollectSelectJobMapper.selectAllByDate(timeZone[0], timeZone[1]);
-            save(list, timeZone[2], "depart_name", "总水量", "value");
+            if (list.size() > 0) {
+                for (Map<String, Object> i : list) {
+                    Map<String, String> map = new HashMap<>();
+                    map.put("departName", i.get("depart_name").toString());
+                    map.put("time", timeZone[2]);
+                    map.put("type", "总水量");
+                    map.put("indicator", "达标率");
+                    map.put("value", i.get("value").toString());
+                    dataCollectInsertJobMapper.insertMonthly(map);
+                }
+            }
             c.setTime(date);
             c.add(Calendar.MONTH, 1); // 月份加1天
             date = c.getTime();
@@ -41,23 +53,84 @@ public class DataSaveTest {
     }
 
     /**
-     * 保存detail_port_maonthly表的封装方法
+     * 字符串匹配的理想情况
      *
-     * @param key1 厂站名称的key
-     * @param type 保存的数据类型
-     * @param key2 数据值的key
+     * @throws ParseException
      */
-    public void save(List<Map<String, Object>> list, String month, String key1, String type, String key2) {
-        if (list.size() > 0) {
-            for (Map<String, Object> i : list) {
-                Map<String, String> map = new HashMap<>();
-                map.put("departName", i.get(key1).toString());
-                map.put("time", month);
-                map.put("type", type);
-                map.put("indicator", "达标率");
-                map.put("value", i.get(key2).toString());
-                dataCollectInsertJobMapper.insertMonthly(map);
+    @Test
+    public void saveNiPortDetailMonthly() throws ParseException {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
+        Date beginDate = dateFormat1.parse("2018-08-01");
+        Date endDate = dateFormat1.parse("2021-11-01");
+        Date date = beginDate;
+        while (!date.equals(endDate)) {
+            String[] timeZone = ckProductDealJob.getTimeZone(date);
+            List<Map<String, Object>> list = dataCollectSelectJobMapper.selectNiByDate(timeZone[0], timeZone[1]);
+            if (list.size() > 0) {
+                Map<String, String> map = matchLastCollectPoint(list);
+                Map<String, Double> ratio = calculateRatio(list, map);
+                for (Map.Entry<String, Double> r : ratio.entrySet()) {
+                    Map<String, String> m = new HashMap<>();
+                    m.put("departName", r.getKey());
+                    m.put("time", timeZone[2]);
+                    m.put("type", "含铬水");
+                    m.put("indicator", "达标率");
+                    m.put("value", String.valueOf(r.getValue()));
+                    dataCollectInsertJobMapper.insertMonthly(m);
+                }
+            }
+            c.setTime(date);
+            c.add(Calendar.MONTH, 1); // 月份加1天
+            date = c.getTime();
+        }
+    }
+
+    /**
+     * 返回优先级最高的采样口
+     *
+     * @param list
+     * @return
+     */
+    public Map<String, String> matchLastCollectPoint(List<Map<String, Object>> list) {
+        Map<String, String> map = new HashMap<>();
+        for (Map<String, Object> m : list) {
+            String port = m.get("collect_point").toString();
+            if (!map.containsKey(port)) {
+                for (int i = index.length - 1; i >= 0; i--) {
+                    if (port.contains(index[i])) {
+                        map.put(m.get("depart_name").toString(), port);
+                        break;
+                    }
+                }
             }
         }
+        return map;
+    }
+
+    /**
+     * 计算达标率
+     *
+     * @param list
+     * @return
+     */
+    public Map<String, Double> calculateRatio(List<Map<String, Object>> list, Map<String, String> map) {
+        Map<String, Double> res = new HashMap<>();
+        for (Map.Entry<String, String> m : map.entrySet()) {
+            double count = 0;
+            int sum = 0;
+            String site = m.getKey();
+            String port = m.getValue();
+            for (Map<String, Object> l : list) {
+                String collectPoint = l.get("collect_point").toString();
+                String departName = l.get("depart_name").toString();
+                if (departName.equals(site) && collectPoint.equals(port)) {
+                    count++;
+                    sum += Integer.parseInt(l.get("is_ok").toString());
+                }
+            }
+            res.put(site, sum / count);
+        }
+        return res;
     }
 }
